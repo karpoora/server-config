2aac4c02a411c41d8657b850f1a3a6bd8bf2d393
Sundarapandian, Karpoora <karpoora.sundarapandian@astrazeneca.com>
2017-10-13 22:39:57 +0530

    initial commit

---
+++ src/main/java/com/learning/springboot/serverconfig/LearningSpringBootServerConfigApplication.java
@@ -0,0 +1,12 @@
+package com.learning.springboot.serverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootServerConfigApplication.class, args);
+	}
+}
