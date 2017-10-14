a24fd56e90c726bc4b4aa4b63c33af62db86af0c
Sundarapandian, Karpoora <karpoora.sundarapandian@astrazeneca.com>
2017-10-15 00:50:59 +0530

    config Service implemented

--- src/main/java/com/learning/springboot/serverconfig/LearningSpringBootServerConfigApplication.java
+++ src/main/java/com/learning/springboot/serverconfig/LearningSpringBootServerConfigApplication.java
@@ -2,8 +2,10 @@ package com.learning.springboot.serverconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class LearningSpringBootServerConfigApplication {
 
 	public static void main(String[] args) {
