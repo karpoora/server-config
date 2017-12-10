38a0a2883d69db94d562ff06c6e5cfb7cd24a57a
Sundarapandian, Karpoora <karpoora.sundarapandian@astrazeneca.com>
2017-12-10 14:31:19 +0530

    added to service registry

--- src/main/java/com/microservices/server/config/MicroserviceServerConfig.java
+++ src/main/java/com/microservices/server/config/MicroserviceServerConfig.java
@@ -3,10 +3,10 @@ package com.microservices.server.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
-// @Enable-config-service - centralized configuration - git
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer
 public class MicroserviceServerConfig {
 
